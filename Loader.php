5a2afd79bdb3117ab2243aee890df73b7ce065ec
cassianocastro <tottenham248@gmail.com>
2021-08-03 12:06:51 -0300

    Criação dos arquivos.

---
+++ Loader.php
@@ -0,0 +1,12 @@
+<?php
+spl_autoload_register(
+    function($className)
+    {
+        $file = str_replace("\\", DIRECTORY_SEPARATOR, $className) . ".php";
+        if ( ! file_exists($file)) {
+            echo "--------ERROR: O seguinte arquivo não existe ou não foi encontrado:\n$file";
+        }
+        require $file;
+    }
+);
+?>
