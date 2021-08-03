524701812e3efd04820fcda546939c161e7042b4
cassianocastro <tottenham248@gmail.com>
2021-08-03 12:08:00 -0300

    Modificação do arquivo raíz.

--- index.php
+++ index.php
@@ -1,40 +1,7 @@
-<!DOCTYPE html>
-<html lang="pt-BR" dir="ltr">
-<head>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="application-name" content="MyApp">
-    <meta name="description" content="Aplicação de teste">
-    <meta name="author" content="Cassiano R.Castro">
-    <title>
-        Aplicação | Home
-    </title>
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap">
-    <!-- <link rel="stylesheet" href="view/css/reset.css">
-    <link rel="stylesheet" href="view/css/global.css">
-    <link rel="stylesheet" href="view/css/header.css">
-    <link rel="stylesheet" href="view/css/main.css">
-    <link rel="stylesheet" href="view/css/footer.css"> -->
-    <base href="view/html/" target="_blank">
-</head>
-<body>
-    <?php include_once './view/html/templates/header.php'; ?>
-    <main>
-        <div>
-            <section>
-                <h2>Bem-vindo</h2>
-                <p>
-                    Olá, seja bem-vindo ao sistema de cadastro <br>
-                    Nesse sisteva você poderá realizar cadastros diversos. <br>
-                    Para isso, utilize o menu ao lado para escolher qual cadastro deseja.
-                </p>
-            </section>
-        </div>
-        <?php include_once './view/html/templates/aside.php'; ?>
-    </main>
-    <?php include_once './view/html/templates/footer.php'; ?>
-</body>
-</html>
+<?php
+require_once './Loader.php';
+
+ini_set('display_errors', 1);
+ini_set('display_startup_erros', 1);
+error_reporting(E_ALL);
+?>
