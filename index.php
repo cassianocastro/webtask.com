941118c80a01657b51247284f6fe9b3b696aa044
Cassiano Castro <development.castro@gmail.com>
2024-02-11 17:40:16 -0300

    Refactor: moved class instanciations to controller/CitiesController.php

--- index.php
+++ index.php
@@ -5,24 +5,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-use controller\CitiesController;
-use view\CityView;
-use model\{ DataBaseConfig, DataBaseConnection, CityDAO };
+$city = [ "name" => "Quintão", "state" => "RS" ];
 
-$config   = new DataBaseConfig("localhost", "mysql", "webTask", "php", "php", 3306);
-$connection = (new DataBaseConnection($config))->getConnection();
-
-$cityDAO        = new CityDAO($connection);
-$cityView       = new CityView();
-$cityController = new CitiesController($cityDAO, $cityView);
-
-$city        = $_POST["city"];
-$wasInserted = $cityController->addCity($city);
-
-if ( $wasInserted )
-{
-    //echo ( $wasInserted ) ? "Registro inserido." : "Não foi possivel inserir.";
-    $cityController->showCities();
-}
+(new controller\CitiesController())->addCity($city);
 
 //require_once "view/html/home.php";
