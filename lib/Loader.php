41a7df0df5ba6d65aa50720bb9bb3c1393335693
Cassiano Castro <development.castro@gmail.com>
2024-02-11 16:25:17 -0300

    Style: format adjust

--- lib/Loader.php
+++ lib/Loader.php
@@ -5,9 +5,12 @@ spl_autoload_register(
     function($className)
     {
         $file = str_replace("\\", DIRECTORY_SEPARATOR, $className) . ".php";
-        if ( ! file_exists($file)) {
+
+        if ( ! file_exists($file) )
+        {
             echo "--------ERROR: O seguinte arquivo não existe ou não foi encontrado:\n$file";
         }
+
         require $file;
     }
 );
