33cfcf2b390bebd8306875ab7f08d8b764f14619
Cassiano Castro <development.castro@gmail.com>
2024-02-11 16:22:45 -0300

    Style: format adjust

--- lib/ConnectionSingleton.php
+++ lib/ConnectionSingleton.php
@@ -1,33 +1,41 @@
 <?php
 namespace lib;
+
 /**
  *
  */
 class ConnectionSingleton
 {
+
     private static $instance;
 
     private function __construct() {}
 
-    private function __clone(){}
+    private function __clone() {}
 
-	public function __wakeup(){
+	public function __wakeup()
+    {
 		throw new Exception("Cannot unserialize Singleton");
 	}
 
     public static function getInstance()
     {
-        if (static::$instance === null) {
-            try {
+        if ( static::$instance === null )
+        {
+            try
+            {
                 static::$instance = new \PDO(
                     "mysql:host=localhost;dbname=webTask",
                     "root",
                     ""
                 );
-            } catch (\PDOException $e) {
+            }
+            catch ( \PDOException $e )
+            {
                 die("Error: " . $e->getMessage());
             }
         }
+
         return static::$instance;
     }
 }
