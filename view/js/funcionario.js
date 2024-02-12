5f9468ea9fa91e17f3b7704cd89c0faa276c3c89
Cassiano Castro <development.castro@gmail.com>
2024-02-12 16:43:30 -0300

    Renamed local vars

--- view/js/funcionario.js
+++ view/js/funcionario.js
@@ -10,19 +10,19 @@ function index()
     const form = document.querySelector("employeeForm");
 
     form.addEventListener("submit", () => {
-        let nome      = form.querySelector("input[name=employee\[name\]]");
-        let matricula = form.querySelector("input[name=employee\[registration\]]");
-        let admissao  = form.querySelector("input[name=employee\[admission\]]");
-        let salario   = form.querySelector("input[name=employee\[wage\]]");
-        let filhos    = form.querySelector("input[name=employee\[sons\]]");
-        let cargo     = form.querySelector("input[name=employee\[office\]]");
+        let name      = form.querySelector("input[name=employee\[name\]]");
+        let register  = form.querySelector("input[name=employee\[registration\]]");
+        let admission = form.querySelector("input[name=employee\[admission\]]");
+        let wage      = form.querySelector("input[name=employee\[wage\]]");
+        let sons      = form.querySelector("input[name=employee\[sons\]]");
+        let office    = form.querySelector("input[name=employee\[office\]]");
 
-        let fail = ! isValid(nome)
-            || ! isValid(matricula)
-            || ! isValid(admissao)
-            || ! isValid(salario)
-            || ! isValid(filhos)
-            || ! isValid(cargo)
+        let fail = ! isValid(name)
+            || ! isValid(register)
+            || ! isValid(admission)
+            || ! isValid(wage)
+            || ! isValid(sons)
+            || ! isValid(office)
         ;
 
         if ( fail )
