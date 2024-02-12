e110c9a42a3c889cfc418360523f8c3755432a64
Cassiano Castro <development.castro@gmail.com>
2024-02-12 16:31:59 -0300

    Function definition: index()

--- view/js/funcionario.js
+++ view/js/funcionario.js
@@ -2,46 +2,54 @@
 
 import { valueFieldIsValid as isValid } from "./functions.js";
 
-let mainForm = document.getElementById("employeeForm");
+/**
+ *
+ */
+function index()
+{
+    let mainForm = document.getElementById("employeeForm");
 
-mainForm.onsubmit  = function (e) {
-    let nome       = document.getElementById("fieldName");
-    let matricula  = document.getElementById("fieldMatricula");
-    let admissao   = document.getElementById("fieldDate");
-    let salario    = document.getElementById("fieldSalario");
-    let filhos     = document.getElementById("fieldFilhos");
-    let cargo      = document.getElementById("fieldCargo");
-    let fieldsOkay = true;
+    mainForm.onsubmit  = function (e) {
+        let nome       = document.getElementById("fieldName");
+        let matricula  = document.getElementById("fieldMatricula");
+        let admissao   = document.getElementById("fieldDate");
+        let salario    = document.getElementById("fieldSalario");
+        let filhos     = document.getElementById("fieldFilhos");
+        let cargo      = document.getElementById("fieldCargo");
+        let fieldsOkay = true;
 
-    if ( ! isValid(nome) )
-    {
-        fieldsOkay = false;
+        if ( ! isValid(nome) )
+        {
+            fieldsOkay = false;
+        }
+
+        if ( ! isValid(matricula) )
+        {
+            fieldsOkay = false;
+        }
+
+        if ( ! isValid(admissao) )
+        {
+            fieldsOkay = false;
+        }
+
+        if ( ! isValid(salario) )
+        {
+            fieldsOkay = false;
+        }
+
+        if ( ! isValid(filhos) )
+        {
+            fieldsOkay = false;
+        }
+
+        if ( ! isValid(cargo) )
+        {
+            fieldsOkay = false;
+        }
+
+        return fieldsOkay;
     }
-
-    if ( ! isValid(matricula) )
-    {
-        fieldsOkay = false;
-    }
-
-    if ( ! isValid(admissao) )
-    {
-        fieldsOkay = false;
-    }
-
-    if ( ! isValid(salario) )
-    {
-        fieldsOkay = false;
-    }
-
-    if ( ! isValid(filhos) )
-    {
-        fieldsOkay = false;
-    }
-
-    if ( ! isValid(cargo) )
-    {
-        fieldsOkay = false;
-    }
-
-    return fieldsOkay;
 }
+
+index();
