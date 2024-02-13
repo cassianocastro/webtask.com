b0145c458da07defaf46eac118de0b48bb8c0833
Cassiano Castro <development.castro@gmail.com>
2024-02-13 15:08:19 -0300

    Restructured HTML

--- view/templates/includes/aside/index.php
+++ view/templates/includes/aside/index.php
@@ -1,14 +1,16 @@
 <aside>
-  <header>
-    <h2>Menu</h2>
-  </header>
+  <div>
+    <header>
+      <h2>Menu</h2>
+    </header>
 
-  <nav>
-    <ul>
-      <li><a href="people.php">Pessoas</a></li>
-      <li><a href="employee.php">Funcionários</a></li>
-      <li><a href="city.php">Cidades</a></li>
-      <li><a href="district.php">Bairros</a></li>
-    </ul>
-  </nav>
+    <nav>
+      <ul>
+        <li><a href="people.php">Pessoas</a></li>
+        <li><a href="employee.php">Funcionários</a></li>
+        <li><a href="city.php">Cidades</a></li>
+        <li><a href="district.php">Bairros</a></li>
+      </ul>
+    </nav>
+  </div>
 </aside>
