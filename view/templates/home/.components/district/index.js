b97556def46e834b3f46051ee8880d8bef23c3a0
Cassiano Castro <development.castro@gmail.com>
2024-02-13 16:17:53 -0300

    Erased function content

--- view/templates/home/.components/district/index.js
+++ view/templates/home/.components/district/index.js
@@ -1,25 +1,11 @@
 "use strict";
 
-import { valueFieldIsValid as isValid } from "./functions.js";
-
 /**
  *
  */
 function index()
 {
-    const form = document.querySelector("#df");
-
-    form.addEventListener("submit", () => {
-        let name = form.querySelector("input[name='district\[name\]']");
-        let city = form.querySelector("input[name='district\[cityName\]']");
-
-        if ( ! isValid(name) || ! isValid(city) )
-        {
-            return false;
-        }
-
-        return true;
-    });
+    // code ...
 }
 
 index();
