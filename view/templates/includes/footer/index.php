2f849aa4da5ab8ef3de32be02dc9215e1844fa1a
Cassiano Castro <development.castro@gmail.com>
2024-02-13 15:45:00 -0300

    Refactor: replaced static content by dynamic

--- view/templates/includes/footer/index.php
+++ view/templates/includes/footer/index.php
@@ -1,5 +1,5 @@
 <footer>
   <div>
-    <p>Copyright (c) 2021 Copyright Holder All Rights Reserved.</p>
+    <p>Copyright (c) <?= date("Y") ?> Copyright Holder All Rights Reserved.</p>
   </div>
 </footer>
