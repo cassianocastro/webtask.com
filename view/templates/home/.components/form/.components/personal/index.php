978eeef931cb78f9f5780db61730ea10fa694519
Cassiano Castro <development.castro@gmail.com>
2024-02-14 13:32:40 -0300

    Added HTML content

---
+++ view/templates/home/.components/form/.components/personal/index.php
@@ -0,0 +1,33 @@
+<fieldset>
+  <legend>Personal Info</legend>
+
+  <label>
+    <span>Nome</span>
+
+    <input type="text" name="" placeholder="John" maxlength="50" required>
+  </label>
+
+  <label>
+    <span>Sobrenome</span>
+
+    <input type="text" name="" placeholder="Doe" maxlength="100" required>
+  </label>
+
+  <label>
+    <span>Idade</span>
+
+    <input type="number" name="" placeholder="18" min="0" required>
+  </label>
+
+  <label>
+    <span>Filhos</span>
+
+    <select name="" required>
+      <option value="0" label="Nenhum" selected></option>
+      <option value="1" label="1 filho"></option>
+      <option value="2" label="2 filhos"></option>
+      <option value="3" label="3 filhos"></option>
+      <option value="4" label="4 filhos ou mais"></option>
+    </select>
+  </label>
+</fieldset>
