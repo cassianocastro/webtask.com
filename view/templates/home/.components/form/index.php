116fb29ddf46182857e1518e21e2d9efdedf2da7
Cassiano Castro <development.castro@gmail.com>
2024-02-14 14:33:57 -0300

    Added sidebar to control which fieldset will be displayed

--- view/templates/home/.components/form/index.php
+++ view/templates/home/.components/form/index.php
@@ -20,11 +20,53 @@
     <footer>
       <div>
         <menu>
-          <li><button form="ef" type="reset" title="Clear fields">Limpar</button></li>
-          <li><button form="ef" type="submit" title="Submit data">Salvar</button></li>
+          <li>
+            <button form="ef" type="reset" title="Clear fields">
+              Limpar
+            </button>
+          </li>
+          <li>
+            <button form="ef" type="submit" title="Submit data">
+              Salvar
+            </button>
+          </li>
         </menu>
       </div>
     </footer>
+
+    <aside>
+      <div>
+        <menu>
+          <li>
+            <button form="ef" type="button" title="">
+              <figure>
+                <img src="view/img/user.png" alt="" height="50" width="50">
+
+                <figcaption>User</figcaption>
+              </figure>
+            </button>
+          </li>
+          <li>
+            <button form="ef" type="button" title="">
+              <figure>
+                <img src="view/img/location.png" alt="" height="50" width="50">
+
+                <figcaption>Address</figcaption>
+              </figure>
+            </button>
+          </li>
+          <li>
+            <button form="ef" type="button" title="">
+              <figure>
+                <img src="view/img/worker.png" alt="" height="50" width="50">
+
+                <figcaption>Work</figcaption>
+              </figure>
+            </button>
+          </li>
+        </menu>
+      </div>
+    </aside>
 
   </div>
 </section>
