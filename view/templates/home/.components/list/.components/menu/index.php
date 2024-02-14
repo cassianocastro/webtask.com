e59f5f6992b442e0b75e229433e46521d6f05fa3
Cassiano Castro <development.castro@gmail.com>
2024-02-14 14:58:46 -0300

    Defined HTML

---
+++ view/templates/home/.components/list/.components/menu/index.php
@@ -0,0 +1,17 @@
+<menu id="context">
+  <li>
+    <button type="button" title="Update register">
+      <span class="material-icons-outlined md-24">edit</span>
+
+      <span>Update</span>
+    </button>
+  </li>
+
+  <li>
+    <button type="button" title="Delete register">
+      <span class="material-icons-outlined md-24">delete</span>
+
+      <span>Remove</span>
+    </button>
+  </li>
+</menu>
