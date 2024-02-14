8389318661b555302399730b4571192ba97763d5
Cassiano Castro <development.castro@gmail.com>
2024-02-13 23:15:08 -0300

    Removed class methods

--- controller/Context.php
+++ controller/Context.php
@@ -1,7 +1,7 @@
 <?php
-namespace controller;
+declare(strict_types=1);
 
-use model\IDAO;
+namespace controller;
 
 /**
  *
@@ -9,21 +9,4 @@ use model\IDAO;
 class Context
 {
 
-    private IDAO $dao;
-
-    public function setStrategy(IDAO $dao): void
-    {
-        $this->dao = $dao;
-    }
-
-    public function insert(array $array): bool
-    {
-        $result = $this->dao->insert($array);
-        return $result;
-    }
-
-    public function getAll(): array
-    {
-        return $this->dao->getAll();
-    }
 }
