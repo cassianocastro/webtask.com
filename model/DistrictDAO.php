daa45e9e11089feef930248d2e5d251d85a32bc4
Cassiano Castro <development.castro@gmail.com>
2024-02-13 23:26:08 -0300

    Removed class methods and attributes

--- model/DistrictDAO.php
+++ model/DistrictDAO.php
@@ -4,34 +4,7 @@ namespace model;
 /**
  *
  */
-class DistrictDAO implements IDAO
+class DistrictDAO
 {
 
-    private $connection;
-
-    public function __construct(DataBaseConnection $connection)
-    {
-        $this->connection = $connection;
-    }
-
-    public function insert(array $array): bool
-    {
-        $statement = $this->connection->prepare(
-            "INSERT INTO district(districtName, cityName) VALUES (?, ?)"
-        );
-        $statement->bindParam(1, $array["name"]);
-        $statement->bindParam(2, $array["cityName"]);
-
-        return $statement->execute();
-    }
-
-    public function getAll(): array
-    {
-        $statement = $this->connection->prepare(
-            "SELECT * FROM district"
-        );
-        $statement->execute();
-
-        return $statement->fetchAll();
-    }
 }
