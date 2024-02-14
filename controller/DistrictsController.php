d32c669ab2c0cbdd04b50db3270c89bf8696eb94
Cassiano Castro <development.castro@gmail.com>
2024-02-13 23:08:01 -0300

    Removed class method

--- controller/DistrictsController.php
+++ controller/DistrictsController.php
@@ -3,26 +3,10 @@ declare(strict_types=1);
 
 namespace controller;
 
-use model\DistrictDAO;
-
 /**
  *
  */
 final class DistrictsController
 {
 
-    public function addDistrict(): void
-    {
-        $district    = $_POST["district"];
-        $districtDAO = new DistrictDAO();
-        $wasInserted = $districtDAO->insert($district);
-        $resultSet   = $districtDAO->getAll();
-
-        $caption = "Bairros cadastrados";
-        $columns = array("districtID", "districtName", "cityName");
-
-        echo ( $wasInserted ) ? "Registro inserido." : "Não foi possivel inserir.";
-
-        include_once '../view/templates/result.php';
-    }
 }
