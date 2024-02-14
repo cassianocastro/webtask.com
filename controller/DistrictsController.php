188e22a4f1da4ca0ff2e73cd146ede196aa00a55
Cassiano Castro <development.castro@gmail.com>
2024-02-13 22:50:18 -0300

    Removed debug function calls

--- controller/DistrictsController.php
+++ controller/DistrictsController.php
@@ -5,10 +5,6 @@ use Model\DistrictDAO;
 
 require_once '../model/DistrictDAO.php';
 
-ini_set('display_errors', 1);
-ini_set('display_startup_erros', 1);
-error_reporting(E_ALL);
-
 $district    = $_POST["district"];
 
 $districtDAO = new DistrictDAO();
