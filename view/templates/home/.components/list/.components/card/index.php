2a5b6ea602dddd52adc96c70a901d2d388aeee3c
Cassiano Castro <development.castro@gmail.com>
2024-02-14 11:35:34 -0300

    Added HTML content

---
+++ view/templates/home/.components/list/.components/card/index.php
@@ -0,0 +1,11 @@
+<div class="item">
+  <figure>
+    <img src="view/img/worker.png" alt="Employee Icon" height="24" width="24">
+
+    <figcaption><?= $employee ?></figcaption>
+  </figure>
+
+  <button type="button" title="Show details">
+    <img src="view/img/spiral.png" alt="Details Icon" height="24" width="24">
+  </button>
+</div>
