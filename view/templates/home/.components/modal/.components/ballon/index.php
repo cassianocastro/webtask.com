aa1e85d6d2474aa3f017298e0d24d385233ad9f9
Cassiano Castro <development.castro@gmail.com>
2024-02-15 16:24:49 -0300

    Added HTML content

---
+++ view/templates/home/.components/modal/.components/ballon/index.php
@@ -0,0 +1,14 @@
+<div class="ballon">
+  <p>
+    Av. dos Industrários, 1384, Navegantes, Porto Alegre/RS
+    <br>
+    CEP 91834-882
+    <br>
+    <br>
+    Em frente ao depósito de mercadorias da Renner.
+  </p>
+
+  <button type="button" class="material-icons" title="Copy">
+    content_copy
+  </button>
+</div>
