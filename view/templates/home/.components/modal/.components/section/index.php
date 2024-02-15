f5a19a670fa4e170e44435868d39585fab1ee09d
Cassiano Castro <development.castro@gmail.com>
2024-02-15 16:24:58 -0300

    Added HTML content

---
+++ view/templates/home/.components/modal/.components/section/index.php
@@ -0,0 +1,90 @@
+<section>
+  <div>
+
+    <header>
+      <div>
+        <h5>Employee's Details</h5>
+      </div>
+    </header>
+
+    <form method="dialog">
+      <div>
+        <fieldset>
+          <label>
+            <span>Matrícula</span>
+
+            <input type="text" value="123456789" readonly>
+          </label>
+
+          <label>
+            <span>Data de Admissão</span>
+
+            <input type="date" value="2018-08-19" readonly>
+          </label>
+
+          <label>
+            <span>Salário</span>
+
+            <input type="text" value="U$ 1500.00" readonly>
+          </label>
+
+          <label>
+            <span>Cargo</span>
+
+            <input type="text" value="Encarregado" readonly>
+          </label>
+        </fieldset>
+      </div>
+    </form>
+
+    <footer>
+      <div>
+        <p>
+          Last updated at <time datetime="2024-02-15 18:31:25">Thu, 15 Feb 2024 18:31:25 GMT</time>
+        </p>
+      </div>
+    </footer>
+
+    <aside>
+      <div>
+        <figure>
+          <img
+            src="view/img/hypnosis.png"
+            alt=""
+            height="70"
+            width="70"
+          >
+
+          <figcaption>
+            <span>Doe, </span>
+
+            <span>John</span>
+          </figcaption>
+        </figure>
+
+        <p>18 years old</p>
+
+        <p>4 son(s)</p>
+
+        <menu>
+          <li>
+            <button type="button" title="Show phones">
+              <span class="material-icons">phone</span>
+
+              <span>Phones</span>
+            </button>
+          </li>
+
+          <li>
+            <button type="button" title="Show address">
+              <span class="material-icons">location_on</span>
+
+              <span>Address</span>
+            </button>
+          </li>
+        </menu>
+      </div>
+    </aside>
+
+  </div>
+</section>
