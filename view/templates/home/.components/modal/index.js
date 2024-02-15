0d00d065fb0fba8a12fbcf1853b2c54149c9e09f
Cassiano Castro <development.castro@gmail.com>
2024-02-15 16:57:43 -0300

    Listeners to header & footer buttons of the modal

--- view/templates/home/.components/modal/index.js
+++ view/templates/home/.components/modal/index.js
@@ -5,7 +5,19 @@
  */
 function index()
 {
-    // code
+    const dialog = document.querySelector("#details");
+
+    {
+        const button = dialog.querySelector("header [title='Close']");
+
+        button.addEventListener("click", () => dialog.close());
+    }
+
+    {
+        const button = dialog.querySelector("footer [title~='Close']");
+
+        button.addEventListener("click", () => dialog.close());
+    }
 }
 
 export { index };
