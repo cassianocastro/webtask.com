bf6fd893789561b1741f24d566f3d73ca20952aa
Cassiano Castro <development.castro@gmail.com>
2024-02-15 16:24:24 -0300

    Moved section and ballon subcomponents to ./.components/{section,ballon}/ directories

--- view/templates/home/.components/modal/index.php
+++ view/templates/home/.components/modal/index.php
@@ -15,89 +15,7 @@
       </div>
     </header>
 
-    <section>
-      <div>
-        <header>
-          <div>
-            <h5>Employee's Details</h5>
-          </div>
-        </header>
-
-        <form method="dialog">
-          <div>
-            <fieldset>
-              <label>
-                <span>Matrícula</span>
-
-                <input type="text" value="123456789" readonly>
-              </label>
-
-              <label>
-                <span>Data de Admissão</span>
-
-                <input type="date" value="2018-08-19" readonly>
-              </label>
-
-              <label>
-                <span>Salário</span>
-
-                <input type="text" value="U$ 1500.00" readonly>
-              </label>
-
-              <label>
-                <span>Cargo</span>
-
-                <input type="text" value="Encarregado" readonly>
-              </label>
-            </fieldset>
-          </div>
-        </form>
-
-        <footer>
-          <div>
-            <p>
-              Last updated at <time datetime="2024-02-15 18:31:25">Thu, 15 Feb 2024 18:31:25 GMT</time>
-            </p>
-          </div>
-        </footer>
-
-        <aside>
-          <div>
-            <figure>
-              <img src="view/img/hypnosis.png" alt="" height="70" width="70">
-
-              <figcaption>
-                <span>Doe, </span>
-
-                <span>John</span>
-              </figcaption>
-            </figure>
-
-            <p>18 years old</p>
-
-            <p>4 son(s)</p>
-
-            <menu>
-              <li>
-                <button type="button" title="Show phones">
-                  <span class="material-icons">phone</span>
-
-                  <span>Phones</span>
-                </button>
-              </li>
-
-              <li>
-                <button type="button" title="Show address">
-                  <span class="material-icons">location_on</span>
-
-                  <span>Address</span>
-                </button>
-              </li>
-            </menu>
-          </div>
-        </aside>
-      </div>
-    </section>
+    <?php require_once __DIR__ . '/.components/section/index.php'; ?>
 
     <footer>
       <div>
@@ -123,16 +41,4 @@
   </div>
 </dialog>
 
-<div class="ballon">
-  <p>
-    Av. dos Industrários, 1384, Navegantes, Porto Alegre/RS
-    <br>
-    CEP 91834-882
-    <br><br>
-    Em frente ao depósito de mercadorias da Renner.
-  </p>
-
-  <button type="button" class="material-icons" title="Copy">
-    content_copy
-  </button>
-</div>
+<?php require_once __DIR__ . '/.components/ballon/index.php'; ?>
