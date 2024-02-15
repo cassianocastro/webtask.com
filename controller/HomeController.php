977b432d11200af21692ff59ce077d4413ea90c0
Cassiano Castro <development.castro@gmail.com>
2024-02-15 18:02:15 -0300

    Declared array to populate view's navigation list

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -11,6 +11,18 @@ final class HomeController
 
     public function showHomepage(): void
     {
+        $employees = [
+            "João da Silva",
+            "João da Silva Sauro",
+            "João da Silva Sauro",
+            "João da Silva Sauro",
+            "João da Silva Sauro",
+            "João da Silva Sauro",
+            "João da Silva Sauro",
+            "João da Silva Sauro",
+            "João da Silva Sauro"
+        ];
+
         require_once __DIR__ . '/../view/templates/home/index.php';
     }
 }
