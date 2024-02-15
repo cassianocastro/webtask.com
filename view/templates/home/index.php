9fe8c0a51b12327dd95757bbe3d87e1dd06f12fa
Cassiano Castro <development.castro@gmail.com>
2024-02-15 17:57:31 -0300

    Changed value of href attribute on HTML link element

--- view/templates/home/index.php
+++ view/templates/home/index.php
@@ -8,7 +8,7 @@
   <meta name="description" content="Aplicação de teste">
   <meta name="author" content="Cassiano Castro">
 
-  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link rel="preconnect" href="https://fonts.googleapis.com/css">
 
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp">
   <link rel="stylesheet" type="text/css" href="view/templates/home/index.css">
