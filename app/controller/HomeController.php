02a6027d68ba243acc4d0fb436f185254ce38832
Cassiano Castro <development.castro@gmail.com>
2024-02-16 15:08:14 -0300

    Updated file extension on string path

--- app/controller/HomeController.php
+++ app/controller/HomeController.php
@@ -25,6 +25,6 @@ final class HomeController
             "João da Silva Sauro"
         ];
 
-        (new View())->render("home/index.php", ["employees" => $employees]);
+        (new View())->render("home/index.twig", ["employees" => $employees]);
     }
 }
