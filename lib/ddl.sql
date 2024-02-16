0240be7338d1e056d1a3f791e93d2dbc302c474a
Cassiano Castro <development.castro@gmail.com>
2024-02-16 18:24:26 -0300

    Added conditional clause on create table statements

--- lib/ddl.sql
+++ lib/ddl.sql
@@ -1,16 +1,16 @@
-CREATE TABLE city (
+CREATE TABLE IF NOT EXISTS city (
     cityID   INT(11) NOT NULL,
     cityName VARCHAR(100) NOT NULL,
     state    VARCHAR(100) NOT NULL
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE district (
+CREATE TABLE IF NOT EXISTS district (
     districtID   INT(11) NOT NULL,
     districtName VARCHAR(100) NOT NULL,
     cityName     VARCHAR(100) NOT NULL
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE employee (
+CREATE TABLE IF NOT EXISTS employee (
     employeeID   INT(11) NOT NULL,
     completeName VARCHAR(300) NOT NULL,
     registration VARCHAR(100) NOT NULL,
@@ -20,7 +20,7 @@ CREATE TABLE employee (
     office       VARCHAR(30) NOT NULL
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE people (
+CREATE TABLE IF NOT EXISTS people (
     personID  INT(11) NOT NULL,
     firstName VARCHAR(50) DEFAULT NULL,
     lastName  VARCHAR(100) DEFAULT NULL,
