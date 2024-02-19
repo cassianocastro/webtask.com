4452367838231819dd9e7b98532237896d1443d2
Cassiano Castro <development.castro@gmail.com>
2024-02-19 18:22:50 -0300

    Replaced static array by retrieved data from the database

--- app/controller/HomeController.php
+++ app/controller/HomeController.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace App\controller;
 
+use App\model\{ ConnectionFactory, DBConfig };
+use App\model\repository\EmployeeRepository;
 use App\view\View;
 
 /**
@@ -13,24 +15,9 @@ final class HomeController
 
     public function showHomepage(): void
     {
-        $employees = [
-            "João da Silva",
-            "João da Silva Sauro",
-            "João da Silva Sauro",
-            "João da Silva Sauro",
-            "João da Silva Sauro",
-            "João da Silva Sauro",
-            "João da Silva Sauro",
-            "João da Silva Sauro",
-            "João da Silva Sauro",
-            "João da Silva Sauro",
-            "João da Silva Sauro",
-            "João da Silva Sauro",
-            "João da Silva Sauro",
-            "João da Silva Sauro",
-            "João da Silva Sauro",
-            "João da Silva Sauro"
-        ];
+        $config     = new DBConfig("localhost", "mysql", "webtask", "php", "php", 3306);
+        $connection = (new ConnectionFactory())->create($config);
+        $employees  = (new EmployeeRepository($connection))->getAll();
 
         (new View())->render("home/index.twig", ["employees" => $employees]);
     }
