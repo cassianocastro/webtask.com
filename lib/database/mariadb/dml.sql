00195ab24d0048e833d9a22fb28604b4f9a13e06
Cassiano Castro <development.castro@gmail.com>
2024-02-19 16:03:49 -0300

    Adjust insert statements

--- lib/database/mariadb/dml.sql
+++ lib/database/mariadb/dml.sql
@@ -1,28 +1,4 @@
 INSERT INTO
-    region (name)
-VALUES
-    ('Norte'),
-    ('Sul'),
-    ('Nordeste'),
-    ('Sudeste'),
-    ('Centro-Oeste');
-
-INSERT INTO
-    state (UF, name, region)
-VALUES
-    ('RS', 'Rio Grande do Sul', 2),
-    ('SP', 'São Paulo', 4),
-    ('RJ', 'Rio de Janeiro', 4);
-
-INSERT INTO
-    address (state, city, district, street, number, cep, complement)
-VALUES
-    ('RS', 'Porto Alegre', 'Sarandi', '', 10, 12345000, NULL),
-    ('RS', 'Porto Alegre', 'Rubem Berta', '', 323, 12345000, NULL),
-    ('RS', 'Porto Alegre', 'Moinhos de Vento', '', 410, 12345000, NULL),
-    ('RS', 'Porto Alegre', 'Cavalhada', '', 600, 12345000, NULL);
-
-INSERT INTO
     contract (registration, admission, wage, office)
 VALUES
     ('001', '2021-07-05', 3000, 'Encarregado'),
@@ -31,9 +7,17 @@ VALUES
     ('004', '2021-07-01', 1500, 'Auxiliar');
 
 INSERT INTO
-    employee (CPF, name, surname, age, sons, address, contract)
+    employee (CPF, name, surname, age, sons, contract)
 VALUES
-    (12345678900, 'José', 'da Silva', 34, 3, 2, 1),
-    (98765432100, 'Ana' , 'Moura'   , 18, 1, 3, 4),
-    (12312312300, 'João', 'da Silva', 43, 2, 1, 2),
-    (45645645600, 'Bia' , 'Barroso' , 27, 0, 2, 3);
+    (12345678900, 'José', 'da Silva', 34, 3, 1),
+    (98765432100, 'Ana' , 'Moura'   , 18, 1, 4),
+    (12312312300, 'João', 'da Silva', 43, 2, 2),
+    (45645645600, 'Bia' , 'Barroso' , 27, 0, 3);
+
+INSERT INTO
+    address (employeeID, uf, city, district, street, number, cep, complement)
+VALUES
+    (12312312300, 'RS', 'Porto Alegre', 'Sarandi', 'Rua 1', 10, 12345000, NULL),
+    (12345678900, 'RS', 'Porto Alegre', 'Rubem Berta', 'Rua 2', 323, 12345000, NULL),
+    (98765432100, 'RS', 'Porto Alegre', 'Moinhos de Vento', 'Av. Farrapos', 410, 12345000, NULL),
+    (45645645600, 'RS', 'Porto Alegre', 'Cavalhada', 'Rua 3', 600, 12345000, NULL);
