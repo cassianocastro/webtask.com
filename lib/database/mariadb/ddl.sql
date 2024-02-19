013641aa045b01bd5e887b68da584acf9cbe2411
Cassiano Castro <development.castro@gmail.com>
2024-02-19 16:33:12 -0300

    Surrounded code in SQL procedure

--- lib/database/mariadb/ddl.sql
+++ lib/database/mariadb/ddl.sql
@@ -1,44 +1,73 @@
-CREATE TABLE IF NOT EXISTS contract (
-    ID           BIGINT UNSIGNED AUTO_INCREMENT,
-    registration VARCHAR(100) NOT NULL,
-    admission    DATE NOT NULL,
-    wage         DECIMAL(11, 2) NOT NULL,
-    office       VARCHAR(30) NOT NULL,
+DROP PROCEDURE IF EXISTS createTables;
 
-    PRIMARY KEY(ID)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+DELIMITER $$
 
-CREATE TABLE IF NOT EXISTS employee (
-    CPF      BIGINT UNSIGNED,
-    name     VARCHAR(50) DEFAULT NULL,
-    surname  VARCHAR(100) DEFAULT NULL,
-    age      INT DEFAULT NULL,
-    sons     SMALLINT UNSIGNED DEFAULT 0,
-    contract BIGINT UNSIGNED NOT NULL,
+CREATE PROCEDURE createTables()
+BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        ROLLBACK;
 
-    FOREIGN KEY (contract)
-    REFERENCES
-        contract(ID)
-    ON UPDATE CASCADE
-    ON DELETE CASCADE,
+        SELECT "Error at create table statements" AS msg;
+    END;
 
-    PRIMARY KEY(CPF)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+    START TRANSACTION;
 
-CREATE TABLE IF NOT EXISTS address (
-    employeeID BIGINT UNSIGNED NOT NULL,
-    uf         CHAR(2) NOT NULL,
-    city       VARCHAR(100) NOT NULL,
-    district   VARCHAR(100) NOT NULL,
-    street     VARCHAR(100) NOT NULL,
-    number     INT NOT NULL,
-    cep        INT NOT NULL,
-    complement TEXT,
+    DROP TABLE IF EXISTS address;
 
-    FOREIGN KEY (employeeID) REFERENCES
-        employee(CPF)
-    ON UPDATE CASCADE
-    ON DELETE CASCADE,
+    DROP TABLE IF EXISTS employee;
 
-    PRIMARY KEY(employeeID, uf, city, district, street, number, cep)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+    DROP TABLE IF EXISTS contract;
+
+    CREATE TABLE contract (
+        ID           BIGINT UNSIGNED AUTO_INCREMENT,
+        registration VARCHAR(100) NOT NULL,
+        admission    DATE NOT NULL,
+        wage         DECIMAL(11, 2) NOT NULL,
+        office       VARCHAR(30) NOT NULL,
+
+        PRIMARY KEY(ID)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
+    CREATE TABLE employee (
+        CPF      BIGINT UNSIGNED,
+        name     VARCHAR(50) DEFAULT NULL,
+        surname  VARCHAR(100) DEFAULT NULL,
+        age      INT DEFAULT NULL,
+        sons     SMALLINT UNSIGNED DEFAULT 0,
+        contract BIGINT UNSIGNED NOT NULL,
+
+        FOREIGN KEY (contract)
+        REFERENCES
+            contract(ID)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+
+        PRIMARY KEY(CPF)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
+    CREATE TABLE address (
+        employeeID BIGINT UNSIGNED NOT NULL,
+        uf         CHAR(2) NOT NULL,
+        city       VARCHAR(100) NOT NULL,
+        district   VARCHAR(100) NOT NULL,
+        street     VARCHAR(100) NOT NULL,
+        number     INT NOT NULL,
+        cep        INT NOT NULL,
+        complement TEXT,
+
+        FOREIGN KEY (employeeID) REFERENCES
+            employee(CPF)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+
+        PRIMARY KEY(employeeID, uf, city, district, street, number, cep)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
+    COMMIT;
+
+END $$
+
+DELIMITER ;
+
+CALL createTables();
--- lib/database/mariadb/dml.sql
+++ lib/database/mariadb/dml.sql
@@ -1,23 +1,46 @@
-INSERT INTO
-    contract (registration, admission, wage, office)
-VALUES
-    ('001', '2021-07-05', 3000, 'Encarregado'),
-    ('002', '2021-07-02', 3000, 'Gerente'),
-    ('003', '2021-07-20', 1000, 'Motorista'),
-    ('004', '2021-07-01', 1500, 'Auxiliar');
+DROP PROCEDURE IF EXISTS createRegisters;
 
-INSERT INTO
-    employee (CPF, name, surname, age, sons, contract)
-VALUES
-    (12345678900, 'José', 'da Silva', 34, 3, 1),
-    (98765432100, 'Ana' , 'Moura'   , 18, 1, 4),
-    (12312312300, 'João', 'da Silva', 43, 2, 2),
-    (45645645600, 'Bia' , 'Barroso' , 27, 0, 3);
+DELIMITER $$
 
-INSERT INTO
-    address (employeeID, uf, city, district, street, number, cep, complement)
-VALUES
-    (12312312300, 'RS', 'Porto Alegre', 'Sarandi', 'Rua 1', 10, 12345000, NULL),
-    (12345678900, 'RS', 'Porto Alegre', 'Rubem Berta', 'Rua 2', 323, 12345000, NULL),
-    (98765432100, 'RS', 'Porto Alegre', 'Moinhos de Vento', 'Av. Farrapos', 410, 12345000, NULL),
-    (45645645600, 'RS', 'Porto Alegre', 'Cavalhada', 'Rua 3', 600, 12345000, NULL);
+CREATE PROCEDURE createRegisters()
+BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        ROLLBACK;
+
+        SELECT "Error at insert statements" AS msg;
+    END;
+
+    START TRANSACTION;
+
+    INSERT INTO
+        contract (registration, admission, wage, office)
+    VALUES
+        ('001', '2021-07-05', 3000, 'Encarregado'),
+        ('002', '2021-07-02', 3000, 'Gerente'),
+        ('003', '2021-07-20', 1000, 'Motorista'),
+        ('004', '2021-07-01', 1500, 'Auxiliar');
+
+    INSERT INTO
+        employee (CPF, name, surname, age, sons, contract)
+    VALUES
+        (12345678900, 'José', 'da Silva', 34, 3, 1),
+        (98765432100, 'Ana' , 'Moura'   , 18, 1, 4),
+        (12312312300, 'João', 'da Silva', 43, 2, 2),
+        (45645645600, 'Bia' , 'Barroso' , 27, 0, 3);
+
+    INSERT INTO
+        address (employeeID, uf, city, district, street, number, cep, complement)
+    VALUES
+        (12312312300, 'RS', 'Porto Alegre', 'Sarandi', 'Rua 1', 10, 12345000, NULL),
+        (12345678900, 'RS', 'Porto Alegre', 'Rubem Berta', 'Rua 2', 323, 12345000, NULL),
+        (98765432100, 'RS', 'Porto Alegre', 'Moinhos de Vento', 'Av. Farrapos', 410, 12345000, NULL),
+        (45645645600, 'RS', 'Porto Alegre', 'Cavalhada', 'Rua 3', 600, 12345000, NULL);
+
+    COMMIT;
+
+END $$
+
+DELIMITER ;
+
+CALL createRegisters();
