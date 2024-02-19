bf5a8c39134a937416f94d453467bc0ef091168f
Cassiano Castro <development.castro@gmail.com>
2024-02-19 15:25:30 -0300

    Changed display strategy

--- public/templates/home/.components/form/index.js
+++ public/templates/home/.components/form/index.js
@@ -5,29 +5,39 @@
  */
 function index()
 {
-    let zIndex = 0;
-
-    const section = document.querySelector("#employee-form");
+    const section  = document.querySelector("#employee-form");
+    const employee = section.querySelector("form > div > :first-child");
+    const address  = section.querySelector("form > div > :nth-child(2)");
+    const personal = section.querySelector("form > div > :last-child");
 
     {
-        const set    = section.querySelector("form > div > :first-child");
         const button = section.querySelector("button[title='Work']");
 
-        button.addEventListener("click", () => set.style.zIndex = ++zIndex);
+        button.addEventListener("click", () => {
+            employee.style.display = "flex";
+            address.style.display  = "none";
+            personal.style.display = "none";
+        });
     }
 
     {
-        const set    = section.querySelector("form > div > :nth-child(2)");
         const button = section.querySelector("button[title='Address']");
 
-        button.addEventListener("click", () => set.style.zIndex = ++zIndex);
+        button.addEventListener("click", () => {
+            employee.style.display = "none";
+            address.style.display  = "grid";
+            personal.style.display = "none";
+        });
     }
 
     {
-        const set    = section.querySelector("form > div > :last-child");
         const button = section.querySelector("button[title='User']");
 
-        button.addEventListener("click", () => set.style.zIndex = ++zIndex);
+        button.addEventListener("click", () => {
+            employee.style.display = "none";
+            address.style.display  = "none";
+            personal.style.display = "flex";
+        });
     }
 }
 
