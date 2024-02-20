063a2d17d96a1d138e7d790ecf1073f6214bc252
Cassiano Castro <development.castro@gmail.com>
2024-02-20 15:16:55 -0300

    Moved to api.webtask.com repository

--- lib/database/mariadb/index.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE IF NOT EXISTS webtask;
-
-USE webtask;
-
-source /var/www/api.webtask.com/lib/database/mariadb/ddl.sql
-
-source /var/www/api.webtask.com/lib/database/mariadb/dml.sql
